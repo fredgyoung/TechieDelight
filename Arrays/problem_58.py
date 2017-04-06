7ace5425bf46df9cbffc8a39c09b36bad8de37ee
jdchiu <jdchiu@qti.qualcomm.com>
2017-04-06 16:35:11 -0700

    Different printing

--- Arrays/problem_58.py
+++ Arrays/problem_58.py
@@ -11,11 +11,11 @@ def bubble_sort(array):
         for i in range(n - 1):
             if array[i] > array[i + 1]:
                 array[i], array[i + 1] = array[i + 1], array[i]
-                #print(array)
-        #print(array)
 
     return array
 
-arr = [3, 5, 8, 4, 1, 9, -2]
-print(arr)
-print(bubble_sort(arr))
+
+if __name__ == '__main__':
+    arr = [3, 5, 8, 4, 1, 9, -2]
+    print(arr)
+    print(bubble_sort(arr))
