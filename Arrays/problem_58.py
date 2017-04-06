7b79ce2b8e38106fe492ce25b6f3b2036ef92a73
Fred Young <fredgyoung@gmail.com>
2017-04-05 21:51:20 -0700

    Initial commit

---
+++ Arrays/problem_58.py
@@ -0,0 +1,21 @@
+"""
+Bubble sort
+"""
+
+
+def bubble_sort(array):
+
+    n = len(array)
+
+    for _ in range(n - 1):
+        for i in range(n - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                print(array)
+        #print(array)
+
+    return array
+
+arr = [3, 5, 8, 4, 1, 9, -2]
+print(arr)
+bubble_sort(arr)
