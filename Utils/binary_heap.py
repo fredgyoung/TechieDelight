fc51a8068e61952b450ada32c08a6e71f1db68c7
fredgyoung <fredgyoung@gmail.com>
2017-04-28 12:50:26 -0700

    coding

--- Utils/binary_heap.py
+++ Utils/binary_heap.py
@@ -3,30 +3,14 @@
 class BinaryHeap:
 
     def __init__(self):
-        self.queue = [None]
+        self.stack = [0]
+        self.current_size = 0
 
-    def push(self, obj):
-        self.queue.append(obj)
-
-    def pop(self):
-        if len(self.queue) > 1:
-            return self.queue.pop()
-        else:
-            return None
-
-    def peek(self):
-        return self.queue[-1]
-
-    def swap(self, parent, child):
-        temp = self.queue[parent]
-        self.queue[parent] = self.queue[child]
-        self.queue[child] = temp
+    def insert(self, obj):
+        self.stack.append(obj)
+        self.current_size += 1
 
 
 if __name__ == '__main__':
 
-    h = BinaryHeap()
-    h.push(3)
-    print(h.peek())
-    print(h.pop())
-    print(h.pop())
+    bh = BinaryHeap()
